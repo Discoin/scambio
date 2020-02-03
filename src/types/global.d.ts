7754bc8211eda6f125d0a2d395b28e3d75931b27
Jonah Snider <jonah@jonah.pw>
2020-02-02 20:25:00 -0800

    chore(tsc): remove AVA compile-time patch
    AVA was updated so this is no longer necessary
    
    Signed-off-by: Jonah Snider <jonah@jonah.pw>

--- src/types/global.d.ts
+++
@@ -1,6 +0,0 @@
-// This is a monkey patch so AVA will compile without errors
-// See this GitHub issue for context https://github.com/avajs/ava/issues/2332
-// See this reply for the source of this code https://github.com/avajs/ava/issues/2332#issuecomment-570442898
-declare interface SymbolConstructor {
-	readonly observable: symbol;
-}
