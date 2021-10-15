dc4e126a67d5045c6462c99eb30b25d2a3c01373
Jonah Snider <jonah@jonahsnider.com>
2021-10-15 01:09:35 -0700

    refactor: comply with lint rules

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -4,14 +4,12 @@ import packageJSON from '../../package.json';
  * Regular expression for a v4 UUUID (used for transaction IDs).
  * @internal
  */
-// eslint-disable-next-line @typescript-eslint/naming-convention
 export const UUID_V4_REG_EXP = /^[\da-f]{8}-[\da-f]{4}-4[\da-f]{3}-[89ab][\da-f]{3}-[\da-f]{12}$/i;
 
 /**
  * The base URL for the API.
  * @internal
  */
-// eslint-disable-next-line @typescript-eslint/naming-convention
 export const API_URL = 'https://discoin.zws.im';
 
 /**
@@ -20,5 +18,4 @@ export const API_URL = 'https://discoin.zws.im';
  * @example 'Scambio v3.0.0'
  * @internal
  */
-// eslint-disable-next-line @typescript-eslint/naming-convention
 export const USER_AGENT = `Scambio v${packageJSON.version}`;
